82d8c22b6ff2d6fac710c2f05183d5d541ac96a2
ngochung207 <ngochung207@gmail.com>
2020-12-03 06:55:42 +0700

    fix code getLengthStock

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-let newP1 = new Product("HH001","00000001","Nokia 1100i",'Vang',200,20,18,'./image/imge_01.png');
-let newP2 = new Product("HH001","00000001","Nokia 1100i",'Vang',200,20,18,'./image/imge_02.png');
+let newP1 = new Product("HH001","00000001","Nokia 1100i",'Vang','./image/imge_01.png','new',200,20,18);
+let newP2 = new Product("HH001","00000001","Nokia 1100i",'Vang','./image/imge_02.png','new',200,20,18);
 
+
+const productManger = new ProductManger()
+
+productManger.addProduct(newP1)
+productManger.addProduct(newP2)
+console.log(productManger.stock)
--- product.js
+++ product.js
@@ -10,7 +10,6 @@ class Product {
         this.price = _price;
         this.cost = _cost
     }
-
 }
 
 class ProductManger {
@@ -18,13 +17,49 @@ class ProductManger {
         this.stock = [];
     }
 
-    /** Thêm sản phẩm mới.
-     *  Kiểm tra mã sản phẩm mới đưa vào đã có trong stock chưa.
-     *  Nếu chưa có thêm mới ngay.
-     *  Nếu đã có thì CỘNG DỒN vào mã đã tồn tại trong kho.
-     */
+    getLengthStock() {
+        // Đếm số lượng hàng hóa trong stock
+        return this.stock.length;
+    }
+
+    getIndexProductInStock(codeProduct) {
+        // Lấy ra thông tin vị trí của sản phẩm trong kho.
+        if (this.checkCodeInStock(codeProduct)) {
+            let size = this.getLengthStock();
+            for (let index = 0; index < size; index++) {
+                if (this.stock[index].code === codeProduct) {
+                    return index;
+                }
+            }
+        }
+        return -1;
+    }
+    getNameProduct(codeProduct) {
+        // Lấy tên sản phẩm từ mã sản phẩm.
+        let i = this.getIndexProductInStock(codeProduct);
+        if (i !== -1){
+            return this.stock[i].name
+        }
+    }
+
+    checkCodeInStock(code){
+        // Kiểm tra mã hàng tồn tại trong kho hay không.
+        let size = this.getLengthStock();
+        for (let index = 0; index < size; index++){
+            if(this.stock[index].code === code){
+                return true;
+            }
+        }
+        return false;
+    }
 
     addProduct(newProduct) {
+        /** Thêm sản phẩm mới.
+         *  Kiểm tra mã sản phẩm mới đưa vào đã có trong stock chưa.
+         *  Nếu chưa có thêm mới ngay.
+         *  Nếu đã có thì CỘNG DỒN vào mã đã tồn tại trong kho.
+         *  ?? sửa lại chỗ này cho phép truyền vào một mảng các sản phẩm
+         */
         if (this.checkCodeInStock(newProduct.code)){
             this.stock.map(element => {
                 if(element.code === newProduct.code){
@@ -34,46 +69,19 @@ class ProductManger {
         } else this.stock.push(newProduct)
     }
 
-    // Xóa sản một sản phẩm. Tham số truyền vào là id sản phẩm
     deleteProduct(codeProduct){
+        // Xóa sản một sản phẩm. Tham số truyền vào là id sản phẩm
         this.stock.split(codeProduct,1);
     }
-    // Lấy tồn kho, trả ra kết quả là số lượng hàng trong kho
+
     getQuantityInStock(codeProduct){
+        // Lấy tồn kho, trả ra kết quả là số lượng hàng trong kho
         let sum = 0;
         if (this.checkCodeInStock(codeProduct)){
             sum = this.stock[this.getIndexProductInStock(codeProduct)].amount;
             return sum;
         } else return sum;
     }
-    // Kiểm tra mã hàng tồn tại trong kho hay không.
-    checkCodeInStock(code){
-        let size = this.stock.length;
-        for (let index = 0; index < size; index++){
-            if(this.stock[index].code === code){
-                return true;
-            }
-        }
-        return false;
-    }
-    // Lấy ra thông tin vị trí của sản phẩm trong kho.
-    getIndexProductInStock(codeProduct) {
-        if (this.checkCodeInStock(codeProduct)) {
-            let size = this.stock.length;
-            for (let index = 0; index < size; index++) {
-                if (this.stock[index].code === codeProduct) {
-                    return index;
-                }
-            }
-        }
-        return -1;
-    }
-    // Lấy tên sản phẩm từ mã sản phẩm.
-    getNameProduct(codeProduct) {
-        let i = this.getIndexProductInStock(codeProduct);
-        if (i !== -1){
-            return this.stock[i].name
-        }
-    }
+
 }
 
