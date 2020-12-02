238d160e031f9aaf32aa44d4d80d6f5def382ce0
ngochung207 <ngochung207@gmail.com>
2020-12-02 09:43:16 +0700

    create project module 1

---
+++ main.js
@@ -0,0 +1,3 @@
+let newP1 = new Product("HH001","00000001","Nokia 1100i",'Vang',200,20,18,'./image/imge_01.png');
+let newP2 = new Product("HH001","00000001","Nokia 1100i",'Vang',200,20,18,'./image/imge_02.png');
+console.log(newP1.name)
---
+++ product.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(_code, _imei, _name, _description, _amount, _price, _cost, _image){
+        this.imei = _imei;
+        this.code = _code;
+        this.name = _name;
+        this.description = _description;
+        this.amount = _amount;
+        this.price = _price;
+        this.cost = _cost;
+        this.image = _image
+    }
+
+}
+
